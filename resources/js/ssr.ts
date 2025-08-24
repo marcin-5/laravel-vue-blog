b2004b238f555f857dfddde9b00b7f668df1c793
Marcin Bojara <marcin_5@mail.ru>
2025-08-24 13:19:18 +0200

    Enhance SSR setup by integrating `ZiggyVue` for route handling on the server side.

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -1,22 +1,24 @@
-import { createInertiaApp } from '@inertiajs/vue3'
-import createServer from '@inertiajs/vue3/server'
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
-import { renderToString } from '@vue/server-renderer'
-import { createSSRApp, h } from 'vue'
-import type { DefineComponent } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3';
+import createServer from '@inertiajs/vue3/server';
+import { renderToString } from '@vue/server-renderer';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import type { DefineComponent } from 'vue';
+import { createSSRApp, h } from 'vue';
+import { ZiggyVue } from 'ziggy-js';
 
 createServer((page) =>
-  createInertiaApp({
-    page,
-    render: renderToString,
-    resolve: (name) =>
-      resolvePageComponent(
-        `./pages/${name}.vue`,
-        import.meta.glob<DefineComponent>('./pages/**/*.vue')
-      ),
-    setup({ App, props, plugin }) {
-      return createSSRApp({ render: () => h(App, props) }).use(plugin)
-    },
-    progress: false,
-  })
-)
+    createInertiaApp({
+        page,
+        render: renderToString,
+        resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
+        setup({ App, props, plugin }) {
+            return createSSRApp({ render: () => h(App, props) })
+                .use(plugin)
+                .use(ZiggyVue, {
+                    ...(page.props as any).ziggy,
+                    location: new URL((page.props as any).ziggy.location),
+                });
+        },
+        progress: false,
+    }),
+);
