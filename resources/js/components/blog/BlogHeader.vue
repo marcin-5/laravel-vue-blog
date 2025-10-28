774de752a3df0597206c396e21b818c935c01272
Marcin Bojara <marcin_5@mail.ru>
2025-10-28 10:08:15 +0100

    Add bottom margin to blog motto in BlogHeader for improved spacing

--- resources/js/components/blog/BlogHeader.vue
+++ resources/js/components/blog/BlogHeader.vue
@@ -16,7 +16,7 @@ const NOTO_SERIF_FONT = "'Noto Serif', serif";
         <h1 class="text-2xl font-bold text-slate-800 dark:text-slate-400">
             {{ blog.name }}
         </h1>
-        <p v-if="displayedMotto" :style="{ fontFamily: NOTO_SERIF_FONT }" class="mt-2 text-gray-800 italic dark:text-gray-200">
+        <p v-if="displayedMotto" :style="{ fontFamily: NOTO_SERIF_FONT }" class="mt-2 mb-12 text-gray-800 italic dark:text-gray-200">
             {{ displayedMotto }}
         </p>
         <div v-if="blog.descriptionHtml" class="prose prose-slate dark:prose-invert max-w-none" v-html="stripCutMarkers(blog.descriptionHtml)" />
