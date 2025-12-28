6d3bf989aeac7e46e131815218f0564f253a40b2
Marcin Bojara <marcin_5@mail.ru>
2025-12-28 13:59:20 +0100

    Update newsletter schedule defaults in configuration

--- config/newsletter.php
+++ config/newsletter.php
@@ -11,9 +11,9 @@ return [
     |
     */
 
-    'daily_weekday_time' => env('NEWSLETTER_DAILY_WEEKDAY_TIME', '14:14'),
-    'daily_weekend_time' => env('NEWSLETTER_DAILY_WEEKEND_TIME', '08:08'),
-    'weekly_day' => env('NEWSLETTER_WEEKLY_DAY', 5), // 5 is Friday
-    'weekly_time' => env('NEWSLETTER_WEEKLY_TIME', '09:09'),
+    'daily_weekday_time' => env('NEWSLETTER_DAILY_WEEKDAY_TIME', '07:07'),
+    'daily_weekend_time' => env('NEWSLETTER_DAILY_WEEKEND_TIME', '11:11'),
+    'weekly_day' => env('NEWSLETTER_WEEKLY_DAY', 7), // 5 is Friday
+    'weekly_time' => env('NEWSLETTER_WEEKLY_TIME', '19:19'),
 
 ];
