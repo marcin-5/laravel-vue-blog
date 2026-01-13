2e497afe57db4764fb76171d030e64165fa8fe37
Marcin Bojara <marcin_5@mail.ru>
2026-01-13 22:10:50 +0100

    Adjust `BorderDivider` margin in Blog Landing layout

--- resources/js/pages/public/blog/Landing.vue
+++ resources/js/pages/public/blog/Landing.vue
@@ -72,7 +72,7 @@ const { mergedThemeStyle } = useBlogTheme(computed(() => props.blog.theme));
                 <main v-if="hasLandingContent" class="min-w-0 flex-1">
                     <div class="prose max-w-none" v-html="landingHtml" />
                 </main>
-                <BorderDivider class="mb-4" />
+                <BorderDivider class="my-4" />
                 <BlogPostsList
                     :blogId="blog.id"
                     :blogSlug="blog.slug"
