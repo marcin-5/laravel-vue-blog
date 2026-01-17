ea0127db320758598a06b3cd907cf5751e9ba163
Marcin Bojara <marcin_5@mail.ru>
2026-01-17 12:41:26 +0100

    Add `Bus::fake` and `SendNewsletterNotification` dispatch assertion to `NewsletterConsolidationTest`

--- tests/Feature/NewsletterConsolidationTest.php
+++ tests/Feature/NewsletterConsolidationTest.php
@@ -1,20 +1,26 @@
 <?php
 
+use App\Jobs\SendNewsletterNotification;
 use App\Mail\NewsletterPostNotification;
 use App\Models\Blog;
 use App\Models\NewsletterSubscription;
 use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Mail;
 
 uses(RefreshDatabase::class);
 
 it('sends separate emails for multiple subscriptions by the same user', function () {
+    Bus::fake();
     Mail::fake();
 
     $email = 'user@example.com';
     $blog1 = Blog::factory()->create(['name' => 'Blog One']);
     $blog2 = Blog::factory()->create(['name' => 'Blog Two']);
+
+    $sendTime = '07:07';
+    $this->travelTo(now()->startOfWeek()->setTimeFromTimeString($sendTime));
 
     NewsletterSubscription::factory()->create([
         'blog_id' => $blog1->id,
@@ -42,6 +48,17 @@ it('sends separate emails for multiple subscriptions by the same user', function
 
     $this->artisan('newsletter:send daily')
         ->assertSuccessful();
+
+    Bus::assertDispatched(SendNewsletterNotification::class, function ($job) use ($email) {
+        if ($job->email !== $email || $job->data->count() !== 2) {
+            return false;
+        }
+
+        // Initiate the task manually to test email sending.
+        $job->handle();
+
+        return true;
+    });
 
     // Now it should send only 1 email
     Mail::assertSent(NewsletterPostNotification::class, 1);
