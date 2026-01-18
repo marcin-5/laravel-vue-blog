4bb65ff1140322ce6fe66178231d8a4a60d2a676
Marcin Bojara <marcin_5@mail.ru>
2026-01-18 11:25:26 +0100

    Update app name configuration to use `LARAVEL_APP_NAME` environment variable

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('LARAVEL_APP_NAME', 'Osobliwy Blog'),
 
     /*
     |--------------------------------------------------------------------------
