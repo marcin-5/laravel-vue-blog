ded227e27eb80ebadc377874a88640b39c14d0a1
Marcin Bojara <marcin_5@mail.ru>
2026-03-29 13:36:43 +0200

    Restore `ProvideMarkdownResponse` middleware to public blog post and landing routes for enhanced markdown response handling.

--- routes/public.php
+++ routes/public.php
@@ -11,6 +11,7 @@ use App\Http\Middleware\SetLocale;
 use App\Http\Middleware\TrackMarkdownRequests;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Support\Facades\Route;
+use Spatie\MarkdownResponse\Middleware\ProvideMarkdownResponse;
 
 // Robots.txt and Sitemap routes (without Inertia and appearance middleware)
 Route::withoutMiddleware([
@@ -51,10 +52,10 @@ Route::post('/newsletter/unsubscribe', [NewsletterController::class, 'unsubscrib
 // Keep these at the very end to avoid conflicts.
 Route::get('{blog:slug}/{postSlug}', [PublicBlogController::class, 'post'])
     ->name('blog.public.post')
-    ->middleware(['track-page-views', TrackMarkdownRequests::class]);
+    ->middleware(['track-page-views', TrackMarkdownRequests::class, ProvideMarkdownResponse::class]);
 
 Route::get('{blog:slug}', [PublicBlogController::class, 'landing'])
     ->name('blog.public.landing')
-    ->middleware(['track-page-views', TrackMarkdownRequests::class]);
+    ->middleware(['track-page-views', TrackMarkdownRequests::class, ProvideMarkdownResponse::class]);
 
 Route::get('/', [PublicHomeController::class, 'welcome'])->name('home');
