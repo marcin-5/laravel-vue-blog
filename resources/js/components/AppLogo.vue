286eeb55f8f20055c48137c9f7bc85f2e52e7a21
Marcin Bojara <marcin_5@mail.ru>
2026-05-14 18:03:02 +0200

    Localize app name in translations and dynamically render it in the AppLogo component.

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import '@fontsource/cinzel-decorative/900.css';
 import '@fontsource/cinzel/700.css';
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
 
 withDefaults(
     defineProps<{
@@ -10,6 +12,10 @@ withDefaults(
         size: 'lg',
     },
 );
+
+const { t } = useI18n();
+const appName = computed(() => t('common.appName'));
+const words = computed(() => appName.value.split(' '));
 </script>
 
 <template>
@@ -18,19 +24,17 @@ withDefaults(
             'inline-block p-2 leading-none font-black tracking-tight text-slate-800 dark:text-slate-200',
             size === 'sm' && 'text-xl sm:text-2xl',
             size === 'md' && 'text-3xl sm:text-4xl',
-            size === 'lg' && 'text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl',
+            size === 'lg' && 'text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl',
         ]"
         style="font-family: 'Cinzel', serif"
     >
-        <span
-            :class="[size !== 'lg' ? 'text-[1.0em]' : 'text-[1.0em]', 'font-black']"
-            style="font-family: 'Cinzel Decorative', serif; font-weight: 900"
-            >O</span
-        >sobliwy
-        <span
-            :class="[size !== 'lg' ? 'text-[1.0em]' : 'text-[1.2em]', 'font-black']"
-            style="font-family: 'Cinzel Decorative', serif; font-weight: 900"
-            >B</span
-        >log
+        <template v-for="(word, index) in words" :key="index">
+            <span
+                :class="[size !== 'lg' ? 'text-[1.0em]' : index !== words.length - 1 ? 'text-[1.0em]' : 'text-[1.2em]', 'font-black']"
+                style="font-family: 'Cinzel Decorative', serif; font-weight: 900"
+                >{{ word[0] }}</span
+            >{{ word.slice(1) }}
+            <template v-if="index < words.length - 1">&nbsp;</template>
+        </template>
     </h1>
 </template>
