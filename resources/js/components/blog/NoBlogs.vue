0d5b7aca70c77ff0586c11f2a5e7560bcae7cec1
Marcin Bojara <marcin_5@mail.ru>
2026-05-17 14:49:29 +0200

    Add multilingual support to "No Blogs" component and update corresponding translations

--- resources/js/components/blog/NoBlogs.vue
+++ resources/js/components/blog/NoBlogs.vue
@@ -1,14 +1,18 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+</script>
 
 <template>
     <div class="mt-20 flex flex-col items-center justify-center gap-4 text-center">
-        <h1 class="text-xl font-semibold text-amber-800 dark:text-amber-50">Witaj</h1>
-        <h2 class="mb-8 text-lg text-orange-800 dark:text-orange-50">Miło, że tu jesteś</h2>
+        <h1 class="text-xl font-semibold text-amber-800 dark:text-amber-50">{{ t('public.no_blogs.welcome') }}</h1>
+        <h2 class="mb-8 text-lg text-orange-800 dark:text-orange-50">{{ t('public.no_blogs.nice_to_see_you') }}</h2>
 
-        <p class="text-base text-orange-900 dark:text-orange-100">Jeszcze nie ma na tej stronie żadnego bloga.</p>
-        <p class="text-base text-orange-900 dark:text-orange-100">Wkrótce to się zmieni i znajdziesz tu ciekawe wpisy.</p>
+        <p class="text-base text-orange-900 dark:text-orange-100">{{ t('public.no_blogs.no_blogs_yet') }}</p>
+        <p class="text-base text-orange-900 dark:text-orange-100">{{ t('public.no_blogs.will_change_soon') }}</p>
 
-        <p class="mt-8 text-orange-950 dark:text-amber-100">Zapraszam wkrótce</p>
+        <p class="mt-8 text-orange-950 dark:text-amber-100">{{ t('public.no_blogs.invite_soon') }}</p>
     </div>
 </template>
 
