33e9dc8de7a0375b843799137a7913dc93484b4c
Marcin Bojara <marcin_5@mail.ru>
2026-05-17 14:57:28 +0200

    Extend domain support in maintenance Caddyfile, refine "No Blogs" translations, and enhance language detection in maintenance page.

--- resources/js/components/blog/NoBlogs.vue
+++ resources/js/components/blog/NoBlogs.vue
@@ -6,13 +6,13 @@ const { t } = useI18n();
 
 <template>
     <div class="mt-20 flex flex-col items-center justify-center gap-4 text-center">
-        <h1 class="text-xl font-semibold text-amber-800 dark:text-amber-50">{{ t('public.no_blogs.welcome') }}</h1>
-        <h2 class="mb-8 text-lg text-orange-800 dark:text-orange-50">{{ t('public.no_blogs.nice_to_see_you') }}</h2>
+        <h1 class="text-xl font-semibold text-amber-800 dark:text-amber-50">{{ t('no_blogs.welcome') }}</h1>
+        <h2 class="mb-8 text-lg text-orange-800 dark:text-orange-50">{{ t('no_blogs.nice_to_see_you') }}</h2>
 
-        <p class="text-base text-orange-900 dark:text-orange-100">{{ t('public.no_blogs.no_blogs_yet') }}</p>
-        <p class="text-base text-orange-900 dark:text-orange-100">{{ t('public.no_blogs.will_change_soon') }}</p>
+        <p class="text-base text-orange-900 dark:text-orange-100">{{ t('no_blogs.no_blogs_yet') }}</p>
+        <p class="text-base text-orange-900 dark:text-orange-100">{{ t('no_blogs.will_change_soon') }}</p>
 
-        <p class="mt-8 text-orange-950 dark:text-amber-100">{{ t('public.no_blogs.invite_soon') }}</p>
+        <p class="mt-8 text-orange-950 dark:text-amber-100">{{ t('no_blogs.invite_soon') }}</p>
     </div>
 </template>
 
