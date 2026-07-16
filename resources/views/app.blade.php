c11a5b0ea54ff81f8c2d70f3dcb2128dec745ed2
Marcin Bojara <marcin_5@mail.ru>
2026-07-16 10:41:13 +0200

    Refactor: simplify `app.blade.php` structure by removing redundant SEO handling and refining asset loading logic

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,12 +21,12 @@
     </script>
 
     {{-- Inline style to set the HTML background color based on our theme in app.css --}}
-    {{-- noinspection CssUnusedSymbol --}}
     <style>
         html {
             background-color: oklch(1 0 0);
         }
 
+        /* noinspection CssUnusedSymbol */
         html.dark {
             background-color: oklch(0.145 0 0);
         }
@@ -37,22 +37,10 @@
     <link rel="icon" href="/{{ $iconsLocale }}/favicon.svg" type="image/svg+xml">
     <link rel="apple-touch-icon" href="/{{ $iconsLocale }}/apple-touch-icon.png">
 
-    {{-- SEO Meta Tags are handled by Inertia Head component in Vue pages via @inertiaHead --}}
-    @if(isset($page['props']['seo']))
-        <title>{{ $page['props']['seo']['title'] ?? config('app.name') }}</title>
-        <meta name="description" content="{{ $page['props']['seo']['description'] ?? '' }}">
-        <meta name="robots" content="{{ $page['props']['seo']['robots'] ?? 'index, follow' }}">
-        @if(isset($page['props']['seo']['canonicalUrl']))
-            <link rel="canonical" href="{{ $page['props']['seo']['canonicalUrl'] }}">
-        @endif
-    @endif
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     @routes
-    @if(app()->environment('testing'))
-        @vite(['resources/js/app.ts'])
-    @else
-        @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
-    @endif
+    @vite(['resources/js/app.ts'])
     @inertiaHead
 </head>
 <body class="antialiased">
